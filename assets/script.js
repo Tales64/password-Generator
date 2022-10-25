8db9cb4270c3f4f07236e4b61a22bc474ac1c1a0
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2022-10-25 01:43:00 -0700

    pseudo code

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 // Assignment Code
 // set up a promp that asks about a password
-
 var generateBtn = document.querySelector("#generate");
-
+// must add a getElementById to the button and a click eventlistener
 var hello = prompt ("Would you like to make a new password?")
  console.log(hello)
+//  dont know if this is the right thing to use
+ if (hello === true){
+  true = prompt ("How many characters would you like in your password?")
+ }
 // promp that askes how many charcters will be needed
 var newPassword = function() {
   var userInput = prompt ("would you like to use lowercase letters?"); 
@@ -22,8 +25,8 @@ var randomNum = (Math.floor(Math.random()*26) +1);
 // prompt that askes if you want special characters
 
 // match generated numbers to selected characters based on answers above
-
-// create a string
+// then push each symbole to an array
+// join all of the generated characters into a single password
 
 // Write password to the #password input
 
