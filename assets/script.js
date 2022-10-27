a57a6750e469cc8d62356ff040e993fc3eb9bb27
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2022-10-26 21:04:55 -0700

    added arrays

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+// creat arrays to pick characters from one for each question
+var num =[1,2,3,4,5,6,7,8,9]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialChar =["!", "@", "#", "$", "%", "^", "&", "*", ":", ";"]
+
 // Assignment Code
 // set up a promp that asks about a password
 var generateBtn = document.querySelector("#generate");
@@ -5,7 +11,8 @@ var generateBtn = document.querySelector("#generate");
 var hello = prompt ("Would you like to make a new password?")
  console.log(hello)
 //  dont know if this is the right thing to use
- if (hello === true){
+function  
+if (hello === true){
   true = prompt ("How many characters would you like in your password?")
  }
 // promp that askes how many charcters will be needed
@@ -17,12 +24,19 @@ var newPassword = function() {
     alert ( "Would you like to make a new Password?")
   }
 
-}
+
 //  prompt that askes if you want uppercase letters 
+if( userInput === true) {
+  alert ( "Would you like to make a new Password?")
+}
 // need a number genrator to get the random characters
 var randomNum = (Math.floor(Math.random()*26) +1);
  console.log(randomNum);
 // prompt that askes if you want special characters
+if( userInput === true) {
+  alert ( "Would you like to make a new Password?")
+}
+}
 
 // match generated numbers to selected characters based on answers above
 // then push each symbole to an array
