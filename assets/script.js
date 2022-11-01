d6f0d5993c77e6b4a10b45daa83385b21fa50de0
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2022-11-01 15:45:57 -0700

    added arrays

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,104 @@
 // creat arrays to pick characters from one for each question
-var num =[1,2,3,4,5,6,7,8,9]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialChar =["!", "@", "#", "$", "%", "^", "&", "*", ":", ";"]
+var numList =["1","2","3","4","5","6","7","8","9"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialCharList =["!", "@", "#", "$", "%", "^", "&", "*", ":", ";"]
 
+var finishedPassword = 0;
 // Assignment Code
 // set up a promp that asks about a password
 var generateBtn = document.querySelector("#generate");
+var x = []
+let newPassword = []
+
+
+
 // must add a getElementById to the button and a click eventlistener
-var hello = prompt ("Would you like to make a new password?")
- console.log(hello)
-//  dont know if this is the right thing to use
-function  
-if (hello === true){
-  true = prompt ("How many characters would you like in your password?")
- }
-// promp that askes how many charcters will be needed
-var newPassword = function() {
-  var userInput = prompt ("would you like to use lowercase letters?"); 
-  console.log(userInput)
-// prompt that asks if you want lower case letters 
-  if( userInput === true) {
-    alert ( "Would you like to make a new Password?")
+function  generatePassword(){
+  var hello = window.confirm ("Would you like to make a new password?")
+  
+  //  dont know if this is the right thing to use
+  if (hello === true){
+    var userInput = window.prompt ("How many characters would you like in your password?")
   }
-
-
-//  prompt that askes if you want uppercase letters 
-if( userInput === true) {
-  alert ( "Would you like to make a new Password?")
+  else{
+    return
+  }
+  console.log(hello)
+  console.log(userInput)
+  
+  // promp that askes how many charcters will be needed
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  var wantNumbers = window.confirm ("would you like to use Numbers?")
+  var wantLowercsae = window.confirm ("would you like to use lowercase letters?") 
+  var wantUppercase = window.confirm ("would you like to use UPPERCASE letters?")
+  var wantSpecial = window.confirm ("would you like to use Special characters?")
+  console.log(wantLowercsae)
+  console.log(wantUppercase)
+  console.log(wantNumbers)
+  console.log(wantSpecial)
+  // prompt that asks if you want lower case letters 
+  var index =[]
+  // match generated numbers to selected characters based on answers above
+  for (let i = 0; i < userInput; i++) {
+    
+    if (wantNumbers === true){
+      var index = Math.floor(Math.random() * numList.length);
+      newPassword += numList[index];
+      
+    }
+    if (newPassword.length -- >= userInput){
+      break;
+    }
+    
+    
+    if (wantLowercsae === true){
+      var index = Math.floor(Math.random() * lowercaseList.length);
+      newPassword += lowercaseList[index];
+    }
+    if (newPassword.length -- >= userInput){
+      break;
+    }
+    
+    console.log(newPassword);
+    if (wantUppercase === true){
+      var index = Math.floor(Math.random() * uppercaseList.length);
+      newPassword += uppercaseList[index];
+    }
+    if (newPassword.length -- >= userInput){
+      break;
+    }
+    
+    console.log(newPassword);
+    if (wantSpecial === true){
+      var index = Math.floor(Math.random() * specialCharList.length);
+      newPassword += specialCharList[index];
+    }
+    console.log(newPassword);
+    if (newPassword.length -- >= userInput){
+      break;
+    }
+  }
+  // console.log(newPassword);
+  // then push each symbole to an array
+  // join all of the generated characters into a single password
+  newPassword
+  // Write password to the #password input
 }
-// need a number genrator to get the random characters
-var randomNum = (Math.floor(Math.random()*26) +1);
- console.log(randomNum);
-// prompt that askes if you want special characters
-if( userInput === true) {
-  alert ( "Would you like to make a new Password?")
-}
-}
-
-// match generated numbers to selected characters based on answers above
-// then push each symbole to an array
-// join all of the generated characters into a single password
-
-// Write password to the #password input
-
+console.log(newPassword)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.value = newPassword;
+  
 }
 
 // Add event listener to generate button
